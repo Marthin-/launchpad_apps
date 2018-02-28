0d384c5559cc85535db65238f9e23521abdf39ee
Marthin- <martin.guilloux@gmail.com>
2018-02-28 17:11:39 +0100

    tictactoe WIP, playing around

---
+++ tictactoe.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+#
+# basic launchpad programming
+# press any button to light its LED 
+# press stop to change color to green
+# press trk on to change to orange
+# press to solo to change to red
+# 
+# press arm to exit
+
+import sys
+
+try:
+	import launchpad_py as launchpad
+except ImportError:
+	try:
+		import launchpad
+	except ImportError:
+		sys.exit("error loading launchpad.py")
+
+import random
+from pygame import time as t
+
+def draw_grid(lp):
+    for i in range(0,8):
+        for j in range(1,9):
+            if (j % 3) == 0:
+                lp.LedCtrlXY(i, j, 3, 3)
+            if ((i+1) % 3) == 0:
+                lp.LedCtrlXY(i,j,3,3)
+
+def main():
+
+### CONNECT ###
+	lp = launchpad.Launchpad();
+	if lp.Open():
+		print("Launchpad Mk1/S/Mini")
+        else:
+            return
+        lp.Reset()
+        lp.ButtonFlush()
+###############
+        status = [0, 0, False]
+
+        mode = 'green'
+
+        board = []
+        for i in range(0,9):
+            board.append(0)
+
+        draw_grid(lp)
+
+        while True:
+            if lp.ButtonChanged() is True:
+                status=lp.ButtonStateXY()
+                print(status)
+                break
+
+#        while True:
+#            if lp.ButtonChanged() is True:
+#                status = lp.ButtonStateXY()
+#                print(status)
+#                if status[0] == 8 and status[1] == 8 and status[2] is False:
+#                    break
+#                elif status[0] == 8 and status[1] == 7:
+#                    mode = 'red'
+#                    lp.LedCtrlXY(status[0], status[1], 3, 0)
+#                    lp.LedCtrlXY(8, 6, 0, 0)
+#                    lp.LedCtrlXY(8, 5, 0, 0)
+#                elif status[0] == 8 and status[1] == 6:
+#                    mode = 'orange' 
+#                    lp.LedCtrlXY(status[0], status[1], 3, 3)
+#                    lp.LedCtrlXY(8, 5, 0, 0)
+#                    lp.LedCtrlXY(8, 7, 0, 0)
+#                elif status[0] == 8 and status[1] == 5:
+#                    mode = 'green' 
+#                    lp.LedCtrlXY(status[0], status[1], 0, 3)
+#                    lp.LedCtrlXY(8, 6, 0, 0)
+#                    lp.LedCtrlXY(8, 7, 0, 0)
+#                else:
+#                    if mode == 'green':
+#                        colors = [0,3]
+#                    elif mode == 'red':
+#                        colors = [3,0]
+#                    else:
+#                        colors = [3,3]
+#                    if status[2] is True:
+#                        lp.LedCtrlXY(status[0], status[1], colors[0], colors[1])
+#
+#        lp.LedCtrlString( 'exit', 3, 3, -1, 50 )
+        t.delay(100)
+	lp.Reset() # turn all LEDs off
+	lp.Close() # close the Launchpad (will quit with an error due to a PyGame bug)
+
+	
+if __name__ == '__main__':
+	main()
